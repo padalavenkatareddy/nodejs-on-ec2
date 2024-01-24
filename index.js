79bcb21e87c6f1e4d5223a25d6c58eb7d3b3f447
padalavenkatareddy <65186428+padalavenkatareddy@users.noreply.github.com>
2024-01-24 15:32:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const app = require("express")();
 app.get("/", (request ,response) => {
-  return response.send(
+  return response.send(`
 
     <h1>
              Hello OPTIT
     </h1>
-  );
+  `);
 });
 
 app.listen(3000, () => console.log("server running"));
