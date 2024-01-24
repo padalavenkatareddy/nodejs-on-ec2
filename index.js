ed0cbe4098fcb0905c34d8ba13ab76048378bfb1
padalavenkatareddy <65186428+padalavenkatareddy@users.noreply.github.com>
2024-01-24 15:38:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-
+// index.js
 const app = require("express")(); 
 app.get("/", (request, response) => { 
-  return response.send(` 
-  <br /> 
-  <br /> 
-  <center> 
+  return response.send(`
+    <br /> 
+    <br /> 
+    <center> 
   <h1> 
-  Hello 👋 from AWS EC2 
+       Hello 👋 from AWS EC2 
   </h1>
   </center>
   `);
 });
+
 app.listen(3000, () => console.log("Server running!!!"));
 
